e2f7366c95cca06f85135b4cbfb73fee15e65b81
JD <jayson.dalpe@gmail.com>
2016-01-26 11:18:16 -0500

    app: src: Refactoring to code standard

--- app/src/main/java/com/thirdbridge/pucksensor/controllers/HomeFragment.java
+++ app/src/main/java/com/thirdbridge/pucksensor/controllers/HomeFragment.java
@@ -26,20 +26,21 @@ import java.util.UUID;
 
 /**
  * Created by Christophe on 2015-10-14.
+ * Modified by Jayson Dalpe since 2016-01-26
  */
 public class HomeFragment extends BaseFragment {
 
-    private Button shotTestButton;
-    private ImageButton newUserImageButton;
-    private Spinner userSpinner;
-    private Button saveUserButton;
-    private Button cancelUserButton;
-    private EditText firstNameEditText;
-    private EditText lastNameEditText;
+    private Button mShotTextButton;
+    private ImageButton mNewUserImageButton;
+    private Spinner mUserSpinner;
+    private Button mSaveUserButton;
+    private Button mCancelUserButton;
+    private EditText mFirstNameEditText;
+    private EditText mLastNameEditText;
 
-    private List<User> users;
-    private List<String> userNames;
-    private ArrayAdapter<User> spinnerAdapter;
+    private List<User> mUsers;
+    private List<String> mUserNames;
+    private ArrayAdapter<User> mSpinnerAdapter;
 
     public static HomeFragment newInstance(){
         return new HomeFragment();
@@ -57,17 +58,17 @@ public class HomeFragment extends BaseFragment {
 
         View v = inflater.inflate(R.layout.fragment_home, container, false);
 
-        shotTestButton = (Button) v.findViewById(R.id.shot_test_button);
-        newUserImageButton = (ImageButton) v.findViewById(R.id.new_user_image_button);
-        userSpinner = (Spinner) v.findViewById(R.id.user_spinner);
+        mShotTextButton = (Button) v.findViewById(R.id.shot_test_button);
+        mNewUserImageButton = (ImageButton) v.findViewById(R.id.new_user_image_button);
+        mUserSpinner = (Spinner) v.findViewById(R.id.user_spinner);
 
         populateSpinner();
 
-        userSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        mUserSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
-                if(users.get(position) != null) {
-                    getController().setCurrentUsername(users.get(position).getFirstName() + " " + users.get(position).getLastName());
+                if (mUsers.get(position) != null) {
+                    getController().setCurrentUsername(mUsers.get(position).getFirstName() + " " + mUsers.get(position).getLastName());
                 }
             }
 
@@ -80,25 +81,25 @@ public class HomeFragment extends BaseFragment {
 
         final PopupWindow popupWindow = inflateNewUserPopup(container);
 
-        newUserImageButton.setOnClickListener(new View.OnClickListener() {
+        mNewUserImageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                ((EditText)popupWindow.getContentView().findViewById(R.id.first_name)).setText("");
-                ((EditText)popupWindow.getContentView().findViewById(R.id.last_name)).setText("");
+                ((EditText) popupWindow.getContentView().findViewById(R.id.first_name)).setText("");
+                ((EditText) popupWindow.getContentView().findViewById(R.id.last_name)).setText("");
                 popupWindow.setFocusable(true);
                 popupWindow.update();
                 popupWindow.showAtLocation(container, Gravity.TOP, 0, 200);
             }
         });
 
-        shotTestButton.setOnClickListener(new View.OnClickListener() {
+        mShotTextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 // Reenable link
-                getController().gotoShotStats((User)userSpinner.getSelectedItem());
-                /*if(userSpinner.getSelectedItemPosition() != -1) {
+                getController().gotoShotStats((User) mUserSpinner.getSelectedItem());
+                /*if(mUserSpinner.getSelectedItemPosition() != -1) {
                     if (getController().isBleDeviceConnected()) {
-                        getController().gotoShotStats((User)userSpinner.getSelectedItem());
+                        getController().gotoShotStats((User)mUserSpinner.getSelectedItem());
                     }
                 }*/
             }
@@ -108,14 +109,14 @@ public class HomeFragment extends BaseFragment {
     }
 
     private void populateSpinner(){
-        users = new ArrayList<>();
+        mUsers = new ArrayList<>();
 
         for(User user: DataManager.get().getUsers()){
-            users.add(user);
+            mUsers.add(user);
         }
 
-        spinnerAdapter = new UserArrayAdapter(getController(), R.layout.spinner_dropdown_item, users);
-        userSpinner.setAdapter(spinnerAdapter);
+        mSpinnerAdapter = new UserArrayAdapter(getController(), R.layout.spinner_dropdown_item, mUsers);
+        mUserSpinner.setAdapter(mSpinnerAdapter);
     }
 
     private PopupWindow inflateNewUserPopup(ViewGroup container){
@@ -126,32 +127,32 @@ public class HomeFragment extends BaseFragment {
 
         final View popupView = layoutInflater.inflate(R.layout.new_user_popup, container, false);
 
-        saveUserButton = (Button) popupView.findViewById(R.id.save_user_button);
-        cancelUserButton = (Button) popupView.findViewById(R.id.cancel_button);
-        firstNameEditText = (EditText) popupView.findViewById(R.id.first_name);
-        lastNameEditText = (EditText) popupView.findViewById(R.id.last_name);
+        mSaveUserButton = (Button) popupView.findViewById(R.id.save_user_button);
+        mCancelUserButton = (Button) popupView.findViewById(R.id.cancel_button);
+        mFirstNameEditText = (EditText) popupView.findViewById(R.id.first_name);
+        mLastNameEditText = (EditText) popupView.findViewById(R.id.last_name);
 
         final PopupWindow popupWindow = new PopupWindow(
                 popupView,
                 1000,
                 ViewGroup.LayoutParams.WRAP_CONTENT);
 
-        saveUserButton.setOnClickListener(new View.OnClickListener() {
+        mSaveUserButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(!firstNameEditText.getText().toString().matches("") || !lastNameEditText.getText().toString().matches("")){
-                    User newUser = new User(UUID.randomUUID().toString(),firstNameEditText.getText().toString(),lastNameEditText.getText().toString());
+                if (!mFirstNameEditText.getText().toString().matches("") || !mLastNameEditText.getText().toString().matches("")) {
+                    User newUser = new User(UUID.randomUUID().toString(), mFirstNameEditText.getText().toString(), mLastNameEditText.getText().toString());
                     DataManager.get().addUser(newUser);
-                    users.add(newUser);
-                    spinnerAdapter.notifyDataSetChanged();
-                    userSpinner.setSelection(spinnerAdapter.getCount() - 1);
+                    mUsers.add(newUser);
+                    mSpinnerAdapter.notifyDataSetChanged();
+                    mUserSpinner.setSelection(mSpinnerAdapter.getCount() - 1);
                     popupWindow.dismiss();
-                    getController().setCurrentUsername(firstNameEditText.getText().toString() + " " + lastNameEditText.getText().toString());
+                    getController().setCurrentUsername(mFirstNameEditText.getText().toString() + " " + mLastNameEditText.getText().toString());
                 }
             }
         });
 
-        cancelUserButton.setOnClickListener(new View.OnClickListener() {
+        mCancelUserButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 popupWindow.dismiss();
